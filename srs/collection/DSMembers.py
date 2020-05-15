ace800e6ce8d1dc0cea2155c3e9f27f1b460f2d0
pablo-mv <64136000+pablo-mv@users.noreply.github.com>
2020-05-15 19:55:40 +0200

    Update DSMembers.py

--- srs/collection/DSMembers.py
+++ srs/collection/DSMembers.py
@@ -1,67 +1,190 @@
-class DNode:
-    def __init__(self,e, nexts = None, prev = None):
-        self.elem = e
+from DSMember import DSMember
+
+from srs.collection.DoublyLinkedList import DList
+
+class DSMember():
+    """
+    Atributes:
+        identifier: Stores an identifier must have the following format: RXXXXXX where X can be any digit
+        name: Stores the name
+        surname: "" "" surname
+        status: Stores whether the member is 'active' or 'inactive'
+        __zones: (Dlist) Contains the zones the member has assignated
+        __packets: (DList) Contains the list of packets to be delivered
+    """
+    def __init__(self, ident, name, surname, status, nexts = None, prev = None):
+        if isinstance(ident, int) and len(str(ident)) <= 6:
+            ceros_extra = 6 - len(str(ident))
+            ceros = ''
+            for _ in range (ceros_extra):
+                ceros += '0'
+            self.__identifier = "R" + ceros +str(ident)
+        else:
+            print("Error - identifier must follow the structure XXXXXX, with X being a number")
+        self.__name = name
+        self.__surname = surname
+        self.__status = status
+        self.__zones = DList()
+        self.__packets = DList()
         self.next = nexts
         self.prev = prev
-        
-        
-#---------------------------------------------------------------------
-class DSMembers:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        self.size = 0
+        surname
     
     def __str__(self):
-        s = '<'
-        if self.isEmpty():
-            s += '>'
-        else:
-            currentNode = self.head
-            while currentNode.next != None:
-                s += str(currentNode.elem) + ', '
-                currentNode = currentNode.next
-            s += str(currentNode.elem)  + '>'
+        """
+        Gives the data of the DSMember for printing
+        Worse case: O(1)
+        Best case: O(1)
+        """
+        s = 'Id: ' + self.__identifier + '\n'
+        s += 'Name: ' + self.__name + '\n'
+        s += 'Surname: ' + self.__surname +'\n'
+        s += 'Status: ' + self.__status  + '\n'
+        s += 'Zones: ' + str(self.__zones) + '\n'
+        s += 'Packets: ' + str(self.__packets)[1:-1] + '\n'
         return s
         
+       
+    def assing_zone(self, zones):
+        """
+        Stores a postal code into a DList
+        Complexity Worse Case: O(n)
+        Complexity Best Case: O(1)
+        """
+        if isinstance(zones, int):
+            self.__zones.addLast(zones)
+            
+        else:
+            print("Error - postal code must be a number")
+            
+    def assign_packet(self, packet):
+        """
+        Stores a package into a DList
+        Complexity Worse Case: O(n)
+        Complexity Best Case: O(1)
+        """
+        self.__packets.addLast(packet)
+        
+    def deliver_packet(self):
+        """
+        Simulates the delivery of a packet
+        Complexity Worse Case: O(1)
+        Complexity Best Case: O(1)
+        """
+        return self.__packets.removeFirst()
+
+    def fullname(self):
+        """
+        Returns the fullname of a DSMember (surname name)
+        Complexity Worse Case: O(1)
+        Complexity Best Case: O(1)
+        """
+        s = self.__surname + ", "  + self.__name 
+        return s
+    
+    def packetSize(self):
+        """
+        Returns the number of packages the current DSMember has.
+        Complexity Worse Case: O(1)
+        Complexity Best Case: O(1)
+        """
+        return self.__packets.size
+    
+    def getPacket(self, idx):
+        """
+        Returns a package into a DList with the index idx
+        Complexity Worse Case: O(n)
+        Complexity Best Case: O(1)
+        """
+        return self.__packets.getAt(idx)
+    
+    @property
+    def identifier(self):
+        return self.__identifier
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def surname(self):
+        return self.__surname
+    
+    @property
+    def status(self):
+        return self.__status
+    @status.setter
+    def status(self, newStatus):
+        self.__status = newStatus
+    
+    @property
+    def zones(self):
+        return self.__zones
+    
+        
+    
+    
+#------------------------------------------------------------------------------
+class DSMembers():
+    def __init__(self):
+        self.__head = None
+        self.__tail = None
+        self.__size = 0
+        
+    def __str__(self):
+        """
+        Gives all of the members information in string form
+        Complexity: Worst O(n), Best O(1)
+        """
+        s = "< \n"
+        if self.isEmpty():
+            s += ">"
+        else:
+            currentNode = self.__head
+            while currentNode != None:
+                s = s + str(currentNode) + ", \n"
+                currentNode = currentNode.next
+            s = s + ">"
+        return s
+    
     def isEmpty(self):
         """
         Returns True if there are not DSMember and False otherwise
         Complexity Best Case: O(1)
         Complexity Worst Case: O(1)
         """
-        return self.size == 0
-        
-    def addFirst(self,e):
+        return self.__size == 0
+    
+    def addFirst(self,ident, name, surname, status):
         """
         Inserts a DSMember in the first place
         Complexity Best Case: O(1)
         Complexity Worst Case: O(1)
         """
-        newNode = DNode(e)
+        newNode = DSMember(ident, name, surname, status)
         if self.isEmpty():
-            self.tail = newNode
-            self.head = newNode
+            self.__head = newNode
+            self.__tail = newNode
         else:
-           newNode.next = self.head
-           self.head.prev = newNode
-           self.head = newNode
-        self.size = self.size + 1
-            
-    def addLast(self,e):
+            newNode.next = self.__head
+            self.__head.prev = newNode
+            self.__head = newNode
+        self.__size = self.__size + 1
+
+    def addLast(self, ident, name, surname, status):
         """
         Inserts a DSMember in the last place
         Complexity Best Case: O(1)
         Complexity Worst Case: O(1)
         """
         if self.isEmpty():
-            self.addFirst(e)
+            self.addFirst(ident, name, surname, status)
         else:
-            newNode = DNode(e)
-            newNode.prev = self.tail
-            self.tail.next = newNode
-            self.tail = newNode
-            self.size = self.size + 1
+            newNode = DSMember(ident, name, surname, status)
+            self.__tail.next = newNode
+            newNode.prev = self.__tail
+            self.__tail = newNode
+            self.__size = self.__size + 1
             
     def removeFirst(self):
         """
@@ -71,14 +194,14 @@ class DSMembers:
         """
         r = None
         if not self.isEmpty():
-            r = self.head.elem
-            self.head = self.head.next
-            if self.size == 1:
-                self.head = None
-                self.tail = None
+            r = self.__head
+            self.__head = self.__head.next
+            if self.__size == 1:
+                self.__head = None
+                self.__tail = None
             else:
-                self.head.prev = None
-            self.size = self.size - 1
+                self.__head.prev = None
+            self.__size = self.__size - 1
         return r
     
     def removeLast(self):
@@ -89,16 +212,16 @@ class DSMembers:
         """
         r = None
         if not self.isEmpty():
-            r = self.tail.elem
-            self.tail = self.tail.prev
-            if self.size == 1:
-                self.head = None
-                self.tail = None
+            r = self.__tail
+            self.__tail = self.__tail.prev
+            if self.__size == 1:
+                self.__head == None
+                self.__tail == None
             else:
-                self.tail.next = None
-            self.size = self.size - 1
+                self.__tail.next = None
+            self.__size = self.__size - 1
         return r
-    
+            
     def getAt(self,n):
         """
         Returns the element of the node of the index given
@@ -107,49 +230,48 @@ class DSMembers:
         """
         r = None
         if n == 0:
-            r = self.head.elem
-        elif n <= self.size:
-            currentNode = self.head
+            r = self.__head
+        elif n == self.__size - 1:
+            r = self.__tail
+        elif n <= self.__size:
+            currentNode = self.__head
             i = 1
-            while i < n:
+            while i <= n:
                 currentNode = currentNode.next
                 i = i + 1
-            
-            r = currentNode.elem
+                
+            r = currentNode
         return r
-    
+        
     def contains(self,e):
         """
         Returns True if the list contains the DSMember, false otherwise
         Complexity Best Case: O(1)
         Complexity Worst Case: O(n)
         """
-        r = -1
-        con = True
-        i = 0
-        currentNode = self.head
-        while i < self.size and con:
-            if currentNode.elem == e:
-                con = False
-                r = i
-            currentNode = currentNode.next
+        r = False
+        currentNode = self.__head
+        while i < self.__size and not r:
+            if currentNode == e:
+                r = True
+            currentNode = currentNode.next()
             i = i + 1
         return r
     
-    def insertAt(self,n,e):
+    def insertAt(self, n, ident, name, surname, status):
         """
         Inserts a DSMember in the position n
         Complexity Best Case: O(1)
         Complexity Worst Case: O(n)
         """
-        if n == 1:
-            self.addFirst(e)
-        elif n >= self.size:
-            self.addLast(e)
+        if n == 0:
+            self.addFirst(ident, name, surname, status)
+        elif n >= self.__size:
+            self.addLast(ident, name, surname, status)
         else:
-            newNode = DNode(e)
+            newNode = DSMember(ident, name, surname, status)
             i = 1
-            currentNode = self.head
+            currentNode = self.__head
             while i < n:
                 currentNode = currentNode.next
                 i = i + 1
@@ -157,86 +279,96 @@ class DSMembers:
             newNode.next = currentNode.next
             currentNode.next.prev = newNode
             currentNode.next = newNode
-            self.size = self.size + 1
-            
+            self.__size = self.__size + 1
+
     def removeAt(self,n):
         """
         Removes the DSMember of the position n and returns it.
         Complexity Best Case: O(1)
         Complexity Worst Case: O(n)
         """
-        if n == 1:
+        if n == 0:
             r = self.removeFirst()
-        elif n == self.size:
+        elif n == self.__size - 1:
             r = self.removeLast()
         else:
             i = 1
-            currentNode = self.head
-            while i < n:
+            currentNode = self.__head
+            while i <= n:
                 currentNode = currentNode.next
                 i = i + 1
-            r = currentNode.elem
-            currentNode.prev.next = currentNode.next
-            currentNode.next.prev = currentNode.prev
-            self.size = self.size - 1
+            r = currentNode
+            prevNode = currentNode.prev
+            nextNode = currentNode.next
+            prevNode.next = nextNode
+            nextNode.prev = prevNode
+            self.__size = self.__size - 1
         return r
-                
+    
     def sortAlphabeticalSurname(self):
         """
         Is a kind of bubble sort with letters.
         Complexity Worse Case: O(n^2)
         Complexity Best Case: O(n^2)
-        """
-        for _ in range(self.size):
-            try:
-                currentNode = self.head
-                nextNode = self.head.next
-                i = 0
-                if nextNode != None:
-                    while nextNode.next != None:
-                        i = 0
-                        while currentNode.elem.fullname()[i] == nextNode.elem.fullname()[i] and i <min(len(currentNode.elem.fullname()), len(nextNode.elem.fullname())-1):
-                            i += 1
-                            """
-                            If the names are repeated, it iterates until get 
-                            an index with a letter diferent or reaching the max size
-                            """
-                        
-                        if currentNode.elem.fullname()[i] > nextNode.elem.fullname()[i]:
-                            aux = currentNode.elem
-                            currentNode.elem = nextNode.elem
-                            nextNode.elem = aux
-                        currentNode = nextNode
-                        nextNode = nextNode.next
-                    
-                    if currentNode.elem.fullname()[i] > nextNode.elem.fullname()[i]:
-                        aux = currentNode.elem
-                        currentNode.elem = nextNode.elem
-                        nextNode.elem = aux
-            except:
-                pass
+        """ 
+        print("Trial -------------------------- \n")
+        currentNode = self.__head
+        for i in range(1, self.__size + 1):
+            for j in range(i):
+                currentNode = currentNode.next
             
-                
-    def removeById(self, identifier):
-        """
-        Removes a DSMember with the identifier the member has. And returns it.
-        Complexity Best Case: O(1)
-        Complexity Worst Case: O(n)
-        """
-        r = None 
-        if self.head.elem.identifier == identifier:
-            r = self.removeFirst()
+            
+            if currentNode.prev != None:
+                con = True #Is the node in the first position?
+                currentSurname = currentNode.surname
+                prevSurname = currentNode.prev.surname
+                while currentSurname < prevSurname and con:
+                    prevNode = currentNode.prev
+                    prev_prev = prevNode.prev
+                    current_next = currentNode.next
+                    currentNode.prev = prev_prev
+                    prevNode.next = current_next
+                    currentNode.next = prevNode
+                    prevNode.prev = currentNode
+                    
+                    if currentNode.prev == None:
+                        con = False
+                        self.__head = currentNode
+                    else:
+                        prevSurname = currentNode.prev.surname
+        currentNode = self.__head
+        while currentNode.next != None:
+            currentNode = currentNode.next
+        self.__tail = currentNode
         
-        elif self.tail.elem.identifier == identifier:
-            r = self.removeLast()
-        elif self.head !=None:
-            currentNode = self.head
-            while currentNode.next != None and r == None:
-                if currentNode.elem.identifier == identifier:
-                    r = currentNode.elem
-                    currentNode.prev.next = currentNode.next
-                    currentNode.next.prev = currentNode.prev
-                else:
-                    currentNode = currentNode.next
-        return r
-                
+                    
+                    
+    
+    
+        
+        
+lis = DSMembers()        
+print(lis.isEmpty())     
+lis.addFirst(1,"Pablo","Morales","active")
+lis.addLast(2,"Cristopher","Manzanos","active")
+lis.insertAt(1,3,"Alejandro", "Pares", "active")
+#print(lis.getAt(0))
+#print("\n")
+#print(lis.getAt(1))
+#print("\n")
+#print(lis.getAt(2))
+#print("-------------------- \n")   
+lis.sortAlphabeticalSurname()
+print("\n")
+print(lis.getAt(0))
+print("\n")  
+print(lis.getAt(1))
+print("\n")
+print(lis.getAt(2))
+
+        
+        
+
+
+        
+        
